f48698263a6931f66305f365fc28e3aafe83db0b
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-11 08:57:37 +0000

    seems that 1.0.0 is untagged. also a readme fix
    git-svn-id: https://plugins.svn.wordpress.org/respondjs/trunk@395898 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- respondjs.php
+++ respondjs.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: Respond js
-Plugin URI: http://www.ramoonus.nl
-Description: Respond.js is a fast & lightweight polyfill for min/max-width CSS3 Media Queries (for IE 6-8, and more).
+Plugin Name: Respond.js
+Plugin URI: http://www.ramoonus.nl/wordpress/respondjs/
+Description: Respond.js is a fast and lightweight polyfill for min/max-width CSS3 Media Queries (for IE 6-8, and more).
 Version: 1.0.0
 Author: Ramoonus
-Author URI: http://www.ramoonus.nl/wordpress/respondjs/
+Author URI: http://www.ramoonus.nl/
 */
 
 // Install
